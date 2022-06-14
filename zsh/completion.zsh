02008a2e31e1d55361cf14455c66a2989d4f968b
yasmin <yasmin@yasminguimaraes.com>
2022-06-13 22:36:51 -0300

    Cleanup

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -2,18 +2,13 @@
 autoload -U compinit
 compinit
 autoload -U +X bashcompinit && bashcompinit
-complete -o nospace -C $HOME/.asdf/installs/terraform/1.0.0/bin/terraform terraform
-
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' insert-tab pending
 
 completions=(
-  "/opt/homebrew/share/zsh/site-functions/_kubectl"
-  "/usr/local/Cellar/asdf/0.10.1/share/zsh/site-functions/_asdf"
+  "/usr/local/Cellar/asdf/0.10.2/share/zsh/site-functions/_asdf"
   "/opt/homebrew/share/zsh/site-functions/_yq"
   "/usr/local/opt/curl/share/zsh/site-functions/curl"
-  "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-  "/Users/lucas.saboya/.pack/completion.zsh"
 )
 
 for completion in ${completions[@]}; do
